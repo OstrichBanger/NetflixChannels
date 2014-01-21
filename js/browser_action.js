05a5399d888ef3778fedee396ab44e75144c9836
OstrichBanger <jonathanost@gmail.com>
2014-01-21 18:07:53 -0500

    Added ability to create and destroy channels

--- js/browser_action.js
+++ js/browser_action.js
@@ -1,6 +1,22 @@
-function addChannel(){
-	document.getElementById("nc-popup").innerHTML("Faggot");
-/*	var channelInput = document.getElementById("channels");
-	var d = document.createElement("button");
-	channelInput.appendChild(d);*/
-}
+$(function() {
+
+	for(var i in localStorage){
+		localKey = localStorage.key(i);
+   		val = localStorage.getItem(i); 
+   		value = "<div class='" + localKey + "'>" + val + "<input type='button' value='remove' id='remove" + localKey + "'></div>";
+		$(".nc-channel-name").append(value);
+		document.getElementById('remove' + localKey).addEventListener('click',function(){
+			localStorage.removeItem(i);
+			$("." + localKey).remove();
+		});
+	};
+
+	document.getElementById('channels').addEventListener('click',function(){
+    	newChannelName = prompt("Enter New Channel Name:", "");
+    	if (newChannelName!=null) {
+    		var nospace = newChannelName.replace(/ /g, '');
+    		localStorage[nospace] = newChannelName;
+    		$(".nc-channel-name").append("<div>",newChannelName,"</div><br>");
+    	}
+  	});
+});
