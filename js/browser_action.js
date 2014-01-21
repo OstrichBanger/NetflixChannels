5c71c55afd973c51dad8206fa7db3a62a8868551
OstrichBanger <jonathanost@gmail.com>
2014-01-21 15:07:57 -0500

    Attempting to get the browser_action javascript to do something.

---
+++ js/browser_action.js
@@ -0,0 +1,6 @@
+function addChannel(){
+	document.getElementById("nc-popup").innerHTML("Faggot");
+/*	var channelInput = document.getElementById("channels");
+	var d = document.createElement("button");
+	channelInput.appendChild(d);*/
+}
