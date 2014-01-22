c8e427ce62278d7695694fe44b9bf3b05f524718
OstrichBanger <jonathanost@gmail.com>
2014-01-21 21:26:22 -0500

    Finalized Pop-up Javascript

--- js/browser_action.js
+++ js/browser_action.js
@@ -3,11 +3,11 @@ $(function() {
 	for(var i in localStorage){
 		localKey = localStorage.key(i);
    		val = localStorage.getItem(i); 
-   		value = "<div class='" + localKey + "'>" + val + "<input type='button' value='remove' id='remove" + localKey + "'></div>";
+   		value = "<div class='chan-names' id='" + localKey + "'>" + val + "<input type='button' class='removeButton' value='x' id='remove" + localKey + "'></div>";
 		$(".nc-channel-name").append(value);
 		document.getElementById('remove' + localKey).addEventListener('click',function(){
 			localStorage.removeItem(i);
-			$("." + localKey).remove();
+			$("#" + localKey).remove();
 		});
 	};
 
@@ -15,8 +15,13 @@ $(function() {
     	newChannelName = prompt("Enter New Channel Name:", "");
     	if (newChannelName!=null) {
     		var nospace = newChannelName.replace(/ /g, '');
-    		localStorage[nospace] = newChannelName;
-    		$(".nc-channel-name").append("<div>",newChannelName,"</div><br>");
+    		localStorage['chan-'+nospace] = newChannelName;
+    		$(".nc-channel-name").append("<div class='chan-name' id='chan-" + nospace + "'>" + newChannelName + "<input type='button' class='removeButton' value='x' id='remove" + nospace + "'></div>");
+    		
+    		document.getElementById('remove' + nospace).addEventListener('click',function(){
+				localStorage.removeItem("chan-"+nospace);
+				$("#chan-" + nospace).remove();
+			});
     	}
   	});
 });
