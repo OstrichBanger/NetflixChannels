08fa54b675b4091fb79312af68ab2319caff6304
OstrichBanger <jonathanost@gmail.com>
2014-01-26 18:53:40 -0500

    can open random episode

--- js/browser_action.js
+++ js/browser_action.js
@@ -8,23 +8,82 @@ $(function() {
    				value = "<div class='chan-names' id='" + key + "'><input type='button' class='playButton' id='play" + key + "'>" + val + "<input type='button' class='removeButton' value='x' id='remove" + key + "'></div>";
 				$(".nc-channel-name").append(value);
 				document.getElementById('play' + key).addEventListener('click',function(){
-					chrome.storage.sync.get(null, function (hrefObj) {
-						console.log(hrefObj);
+					var showToPlay = null;
+					var promiseDone;
+					chrome.storage.sync.get(channelNoSpace + '-shows', function (channelShows) {
+						var showArr = channelShows[channelNoSpace + '-shows'].split(',');
+						showToPlay = showArr[Math.floor((Math.random()*showArr.length))];
+						console.log(showToPlay);
 						var hrefs = [];
-						for (var key in hrefObj){
-   							var yesorno = result[key];
-   							var test = (key.indexOf(channelNoSpace) == 0) && key.substring(key.length-12) == '-morethanone';
-   							if (test && yesorno == 1) {
-   								playLinks = playLinks.replace(/\[\"/g,'');
-   								playLinks = playLinks.replace(/\"\]/g,'');
-   								playLinks = playLinks.replace(/\"\,\"/g,',');
-   								playLinks = playLinks.split(',');
-   								for (var i in playLinks){
-   									hrefs.push(playLinks[i]);
-   								}
-   							}
-   						}
-   						console.log(hrefs);
+						console.log(showToPlay);
+						var morethanone = channelNoSpace + '-' + showToPlay + '-morethanone';
+						console.log(morethanone);
+						chrome.storage.sync.get(morethanone, function (hrefObj) {
+							console.log(hrefObj);
+							var yesorno = hrefObj[morethanone];
+							if (yesorno == 1) {
+								console.log(showToPlay);
+								var storageGetIds = channelNoSpace + '-' + showToPlay + '-dataVid';
+								chrome.storage.sync.get(storageGetIds, function (dataVids) {
+									console.log(dataVids);
+									var dataVidsArr = dataVids[storageGetIds].split(',');
+									console.log(dataVidsArr.length);
+									var storageGetHref = channelNoSpace + '-' + showToPlay + '-seriesHref';
+									chrome.storage.sync.get(storageGetHref, function (seriesHref) {
+										var count = 0;
+										dataVidsArr.forEach(function (i) {
+											console.log(seriesHref);
+							        		var episodeList = [];
+							            	var asyncJSON = $.getJSON(seriesHref[storageGetHref] + "&actionMethod=seasonDetails&seasonId=" + i + "&seasonKind=ELECTRONIC", function(data,textStatus) {
+							                	var stringData = data.html;
+							                	stringData = stringData.replace(/(\r\n|\n|\r)/gm,"");
+							                	stringData = stringData.replace(/\>\s+\</g,'\>\<');
+							                	stringData = stringData.replace(/\s{3,}/g,'');
+							                	// stringData = stringData.replace(/&/g,"&amp;");
+							                	stringData = stringData.match(/<ul.*ul>/g);
+							                	episodeList.push(stringData);
+							            	});
+							            	promiseDone = asyncJSON.promise().done(function () {
+												var test2 = JSON.stringify(episodeList[0]);
+												test2 = test2.replace(/\[\"/g,"");
+												test2 = test2.replace(/\"\]/g,"");
+												test2 = test2.replace(/\\\"/g,'"');
+												$(test2).find("a").each(function () {
+													var test1 = $(this);
+													hrefs.push($(this).attr('href'));
+												});
+												++count;
+												if (count == dataVidsArr.length){
+													var newTab = {};
+													newTab['url'] = hrefs[Math.floor((Math.random()*hrefs.length))];
+													chrome.tabs.create(newTab);
+												}
+											});
+							        	});
+									});
+								});
+							} else {
+				        		var storageGetHref = channelNoSpace + '-' + showToPlay + '-showURL';
+				        		console.log(storageGetHref);
+								chrome.storage.sync.get(storageGetHref, function (showURL) {
+									console.log(showURL[storageGetHref]);
+				        			promiseDone = $.get(showURL[storageGetHref], function(data) {
+							        	data = data.replace(/(\r\n|\n|\r)/gm,"");
+							        	data = data.replace(/\>\s+\</g,'\>\<');
+							        	data = data.replace(/\s{3,}/g,'');
+							        	data = data.replace(/\\n/g,'');
+							        	data = data.replace(/\\\"/g,'"');
+						        		$(data).find(".episodeList a").each(function () {
+						       		    	hrefs.push($(this).attr('href'));
+						        		});
+						        		console.log(hrefs);
+						        		var newTab = {};
+										newTab['url'] = hrefs[Math.floor((Math.random()*hrefs.length))];
+										chrome.tabs.create(newTab);
+									});
+				        		});
+							}
+						});
 					});
 				});
 				document.getElementById('remove' + key).addEventListener('click',function(){
@@ -42,6 +101,7 @@ $(function() {
     		var keyName = 'chan-' + nospace;
     		var Obj = {};
     		Obj[keyName]=newChannelName;
+    		Obj[nospace + '-shows']= null;
     		chrome.storage.sync.set(Obj);
     		$(".nc-channel-name").append("<div class='chan-names' id='"+keyName+"'><input type='button' class='playButton' id='play" + keyName + "'>" + newChannelName + "<input type='button' class='removeButton' value='x' id='remove" + nospace + "'></div>");
     		document.getElementById('play' + keyName).addEventListener('click',function(){
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -15,6 +15,27 @@ $(document).ready(function() {
    						chrome.storage.sync.remove(checkKey);
    						var checkKeyUrls = checkKey + "-urls";
    						chrome.storage.sync.remove(checkKeyUrls);
+   						chrome.storage.sync.remove(checkKey + '-dataVid');
+						chrome.storage.sync.remove(checkKey + '-morethanone');
+						chrome.storage.sync.remove(checkKey + '-seriesHref');
+						chrome.storage.sync.remove(checkKey + '-showURL');
+						var getShows = key.substring(5) + '-shows';
+						chrome.storage.sync.get(getShows,function (showNames){
+							var channelShows = showNames[getShows];
+							var showObj = {};
+							if (channelShows.indexOf(',') != -1){
+								if (channelShows.indexOf(showName) != (channelShows.length - showName.length)) {
+									showObj[getShows] = channelShows.replace(showName + ',', '');
+								}
+								else {
+									showObj[getShows] = channelShows.replace(',' + showName, '');
+								}
+							}
+							else {
+								showObj[getShows] = null;
+							}
+							chrome.storage.sync.set(showObj);
+						});
    					});
    				}		
    				else {
@@ -23,6 +44,20 @@ $(document).ready(function() {
 					document.getElementById(key).addEventListener('click',function () {
 						Obj = {};
 						Obj[key.substring(5) + '-' + showName] = showName.replace(/_/g, " ");
+						var getShows = key.substring(5) + '-shows';
+						console.log(getShows);
+						chrome.storage.sync.get(getShows, function (showNames) {
+							var showObj = {};
+							console.log(showNames);
+							if (showNames[getShows] == null) {
+								showObj[getShows] = showName;
+							}
+							else {
+								showObj[getShows] = showNames[getShows] + ',' + showName;
+							}
+							console.log(showObj);
+							chrome.storage.sync.set(showObj);
+						});
 						console.log(Obj);
 						chrome.storage.sync.set(Obj);
 						document.getElementById(key).innerHTML="<span class='inr'>Added to " + val + "</span>";
@@ -48,6 +83,7 @@ $(document).ready(function() {
 							console.log(dataVid);
 							seasonStorage[checkKey + '-dataVid']=dataVid;
 							seasonStorage[checkKey + '-morethanone']=1;
+							seasonStorage[checkKey + '-seriesHref']=seriesHref;
 							chrome.storage.sync.set(seasonStorage);
 							chrome.storage.sync.get(null, function (resulting) {
 								console.log(resulting);
@@ -62,59 +98,6 @@ $(document).ready(function() {
 								console.log(resulting);
 							});
 						}
-
-					    var hrefs = [];
-						var promiseDone;
-/*					    if(seasons.length) {
-					        // multiple seasons listed, load each tab and get the episodes					        
-					        seasons.each(function (i,e) {
-					        	var episodeList = [];
-					            var asyncJSON = $.getJSON(seriesHref + "&actionMethod=seasonDetails&seasonId=" + $(e).attr("data-vid") + "&seasonKind=ELECTRONIC", function(data,textStatus) {
-					                var stringData = data.html;
-					                stringData = stringData.replace(/(\r\n|\n|\r)/gm,"");
-					                stringData = stringData.replace(/\>\s+\</g,'\>\<');
-					                stringData = stringData.replace(/\s{3,}/g,'');
-					                // stringData = stringData.replace(/&/g,"&amp;");
-					                stringData = stringData.match(/<ul.*ul>/g);
-					                episodeList.push(stringData);
-					            });
-					            promiseDone = asyncJSON.promise().done(function () {
-									var test2 = JSON.stringify(episodeList[0]);
-									test2 = test2.replace(/\[\"/g,"");
-									test2 = test2.replace(/\"\]/g,"");
-									test2 = test2.replace(/\\\"/g,'"');
-									/* console.log(test2); 
-									$(test2).find("a").each(function () {
-										var test1 = $(this);
-										hrefs.push($(this).attr('href'));
-									});
-								});
-					        });
-						
-							promiseDone.promise().done(function () {
-								var showHREFS = JSON.stringify(hrefs);
-								var showStorageKey = checkKey + "-urls";
-								var urlObj = {};
-								urlObj[showStorageKey] = showHREFS;
-								console.log(urlObj);
-								chrome.storage.sync.set(urlObj);
-								chrome.storage.sync.get(null, function (gaysults){
-									console.log(gaysults);
-								});
-							});
-						
-					    } else {
-					        // just one season, no need to load more tabs
-					        $("#seasonDetail").find("a").each(function () {
-								console.log($(this));
-					            hrefs.push($(this).attr('href'));
-					        });
-							var showHREFS = JSON.stringify(hrefs);
-							var showStorageKey = checkKey + "-urls";
-							var urlObj = {};
-							urlObj[showStorageKey] = showHREFS;
-							chrome.storage.sync.set(urlObj);
-					    } */
 					});
 				}
     		}
