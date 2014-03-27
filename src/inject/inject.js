370fde02313b8de1b404cf6080be9132d6fea154
OstrichBanger <jonathanost@gmail.com>
2014-03-26 21:55:33 -0400

    update to 0.0.3, fixed multiple channel issue

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     var showURL = document.URL;
 	var splitURL = showURL.split("/");
 	var showName = splitURL[splitURL.length - 2];
+	var counter = 0;
 	chrome.storage.sync.get(null, function (result) {
     	for (var key in result){
    			val = result[key];
@@ -9,95 +10,104 @@ $(document).ready(function() {
    				var containCheck = Object.keys(result);
    				var checkKey = key.substring(5) + '-' + showName;
    				if (containCheck.indexOf(checkKey) > -1) {
-   					var addButton = "<span class='addChannel btnWrap mltBtn mltBtn-s60'><a id='" + key + "' class='svf-button svfb-silver addlk evo-btn save2add svf-button-add'><span class='remove'>Remove from " + val + "</span></a></span>";
+   					var addButton = "<span class='addChannel btnWrap mltBtn mltBtn-s60'><a id='" + key + "' class='chandel svf-button svfb-silver addlk evo-btn save2add svf-button-add'><span class='remove'>Remove from " + val + "</span></a></span>";
    					$('.actions').append(addButton);
-   					document.getElementById(key).addEventListener('click',function () {
-   						chrome.storage.sync.remove(checkKey);
-   						var checkKeyUrls = checkKey + "-urls";
-   						chrome.storage.sync.remove(checkKeyUrls);
-   						chrome.storage.sync.remove(checkKey + '-dataVid');
-						chrome.storage.sync.remove(checkKey + '-morethanone');
-						chrome.storage.sync.remove(checkKey + '-seriesHref');
-						chrome.storage.sync.remove(checkKey + '-showURL');
-						var getShows = key.substring(5) + '-shows';
-						chrome.storage.sync.get(getShows,function (showNames){
-							var channelShows = showNames[getShows];
-							var showObj = {};
-							if (channelShows.indexOf(',') != -1){
-								if (channelShows.indexOf(showName) != (channelShows.length - showName.length)) {
-									showObj[getShows] = channelShows.replace(showName + ',', '');
+   					$(".chandel").each(function () {
+						var key = $(this).attr('id');
+						var val = key.split('-')[1];
+						var checkKey = key.substring(5) + '-' + showName;
+						document.getElementById(key).addEventListener('click',function () {
+							chrome.storage.sync.remove(checkKey);
+							var checkKeyUrls = checkKey + "-urls";
+							chrome.storage.sync.remove(checkKeyUrls);
+							chrome.storage.sync.remove(checkKey + '-dataVid');
+							chrome.storage.sync.remove(checkKey + '-morethanone');
+							chrome.storage.sync.remove(checkKey + '-seriesHref');
+							chrome.storage.sync.remove(checkKey + '-showURL');
+							var getShows = key.substring(5) + '-shows';
+							console.log(getShows);
+							chrome.storage.sync.get(getShows,function (showNames){
+								var channelShows = showNames[getShows];
+								var showObj = {};
+								if (channelShows.indexOf(',') != -1){
+									if (channelShows.indexOf(showName) != (channelShows.length - showName.length)) {
+										showObj[getShows] = channelShows.replace(showName + ',', '');
+									}
+									else {
+										showObj[getShows] = channelShows.replace(',' + showName, '');
+									}
 								}
 								else {
-									showObj[getShows] = channelShows.replace(',' + showName, '');
+									showObj[getShows] = null;
 								}
-							}
-							else {
-								showObj[getShows] = null;
-							}
-							chrome.storage.sync.set(showObj);
+								chrome.storage.sync.set(showObj);
+								document.getElementById(key).innerHTML="<span class='inr'>Removed from " + val + "</span>";
+							});
 						});
-   					});
+					});
    				}		
    				else {
-    				var addButton = "<span class='addChannel btnWrap mltBtn mltBtn-s60'><a id='" + key + "' class='svf-button svfb-silver addlk evo-btn save2add svf-button-add'><span class='inr'>Add to " + val + "</span></a></span>";
+    				var addButton = "<span class='addChannel btnWrap mltBtn mltBtn-s60'><a id='" + key + "' class='chanadd svf-button svfb-silver addlk evo-btn save2add svf-button-add'><span class='inr'>Add to " + val + "</span></a></span>";
 					$('.actions').append(addButton);
-					document.getElementById(key).addEventListener('click',function () {
-						Obj = {};
-						Obj[key.substring(5) + '-' + showName] = showName.replace(/_/g, " ");
-						var getShows = key.substring(5) + '-shows';
-						console.log(getShows);
-						chrome.storage.sync.get(getShows, function (showNames) {
-							var showObj = {};
-							console.log(showNames);
-							if (showNames[getShows] == null) {
-								showObj[getShows] = showName;
-							}
-							else {
-								showObj[getShows] = showNames[getShows] + ',' + showName;
-							}
-							console.log(showObj);
-							chrome.storage.sync.set(showObj);
-						});
-						console.log(Obj);
-						chrome.storage.sync.set(Obj);
-						document.getElementById(key).innerHTML="<span class='inr'>Added to " + val + "</span>";
-						var seasons = null;
-					    var seriesHref = window.location.href.replace("#","");
-					    seasons = $("#seasonsNav .seasonItem a");
+					$(".chanadd").each(function () {
+						var key = $(this).attr('id');
+						var val = key.split('-')[1];
+						document.getElementById(key).addEventListener('click',function () {
+							Obj = {};
+							Obj[key.substring(5) + '-' + showName] = showName.replace(/_/g, " ");
+							var getShows = key.substring(5) + '-shows';
+							console.log(getShows);
+							chrome.storage.sync.get(getShows, function (showNames) {
+								var showObj = {};
+								if (showNames[getShows] == null) {
+									showObj[getShows] = showName;
+								}
+								else {
+									showObj[getShows] = showNames[getShows] + ',' + showName;
+								}
+								chrome.storage.sync.set(showObj);
+							});
+							console.log(Obj);
+							chrome.storage.sync.set(Obj);
+							document.getElementById(key).innerHTML="<span class='inr'>Added to " + val + "</span>";
+							var seasons = null;
+						    var seriesHref = window.location.href.replace("#","");
+						    seasons = $("#seasonsNav .seasonItem a");
 
-						if(seasons.length) {
-							var seasonStorage = {};
-							var dataVid;
-							var q = 0;
-							seasons.each(function (i,e){
-								var j = $(e).attr("data-vid");
-								if (q == 0) {
-									dataVid = j;
-									q = 1;
-								}
-								else{
-									dataVid += ',';
-									dataVid += j;
-								}
-							});
-							console.log(dataVid);
-							seasonStorage[checkKey + '-dataVid']=dataVid;
-							seasonStorage[checkKey + '-morethanone']=1;
-							seasonStorage[checkKey + '-seriesHref']=seriesHref;
-							chrome.storage.sync.set(seasonStorage);
-							chrome.storage.sync.get(null, function (resulting) {
-								console.log(resulting);
-							});
-						}
-						else{
-							var seasonStorage = {};
-							seasonStorage[checkKey + '-morethanone']=0;
-							seasonStorage[checkKey + '-showURL']=document.URL;
-							chrome.storage.sync.set(seasonStorage);
-							chrome.storage.sync.get(null, function (resulting) {
-								console.log(resulting);
-							});
-						}
+							if(seasons.length) {
+								var seasonStorage = {};
+								var dataVid;
+								var q = 0;
+								seasons.each(function (i,e){
+									var j = $(e).attr("data-vid");
+									if (q == 0) {
+										dataVid = j;
+										q = 1;
+									}
+									else{
+										dataVid += ',';
+										dataVid += j;
+									}
+								});
+								console.log(dataVid);
+								seasonStorage[checkKey + '-dataVid']=dataVid;
+								seasonStorage[checkKey + '-morethanone']=1;
+								seasonStorage[checkKey + '-seriesHref']=seriesHref;
+								chrome.storage.sync.set(seasonStorage);
+								chrome.storage.sync.get(null, function (resulting) {
+									console.log(resulting);
+								});
+							}
+							else{
+								var seasonStorage = {};
+								seasonStorage[checkKey + '-morethanone']=0;
+								seasonStorage[checkKey + '-showURL']=document.URL;
+								chrome.storage.sync.set(seasonStorage);
+								chrome.storage.sync.get(null, function (resulting) {
+									console.log(resulting);
+								});
+							}
+						});
 					});
 				}
     		}
